4378e34cb23deba29f9502024162189fd8fd6d05
Spectraorder <2421036333@qq.com>
2024-02-08 15:25:44 -0500

    control bar for airborne | mask | activity

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,22 @@ var map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+var airborneRateControl = document.getElementById('airborne-rate');
+var maskRateControl = document.getElementById('mask-rate');
+var activityRateControl = document.getElementById('activity-rate');
+
+function handleControlChange() {
+    var airborneRate = parseFloat(airborneRateControl.value);
+    var maskRate = parseFloat(maskRateControl.value);
+    var activityRate = parseFloat(activityRateControl.value);
+
+    console.log("Airborne Rate:", airborneRate);
+    console.log("Mask Rate:", maskRate);
+    console.log("Activity Rate:", activityRate);
+}
+
+airborneRateControl.addEventListener('input', handleControlChange);
+maskRateControl.addEventListener('input', handleControlChange);
+activityRateControl.addEventListener('input', handleControlChange);
