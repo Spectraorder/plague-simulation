904dc8ab9268567f1a8fcabd231c2de2683d1a80
Spectraorder <2421036333@qq.com>
2024-02-09 12:01:23 -0500

    heatmap plugin

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,24 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// var heatData = generateRandomHeatData(1000); // Generate 1000 random data points
+
+// // Create heatmap layer
+// var heat = L.heatLayer(heatData, {radius: 25, gradient: {0.2: 'blue', 0.5: 'lime', 0.7: 'yellow', 1: 'red'}}).addTo(map);
+
+// // Function to generate random data points
+// function generateRandomHeatData(count) {
+//     var data = [];
+//     for (var i = 0; i < count; i++) {
+//         data.push([
+//             Math.random() * 180 - 90, // Random latitude between -90 and 90
+//             Math.random() * 360 - 180, // Random longitude between -180 and 180
+//             Math.random() // Random intensity between 0 and 1
+//         ]);
+//     }
+//     return data;
+// }
+
 
 var airborneRateControl = document.getElementById('airborne-rate');
 var maskRateControl = document.getElementById('mask-rate');
