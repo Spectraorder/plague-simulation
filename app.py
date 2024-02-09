a6d4d5ee4b09856a3d283e07291513972351025a
Spectraorder <2421036333@qq.com>
2024-02-09 11:30:28 -0500

    3 input to backend with start-simulation

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route('/start-simulation', methods=['POST'])
+def start_simulation():
+    data = request.json
+
+    airborne_rate = data.get('airborneRate')
+    mask_rate = data.get('maskRate')
+    activity_rate = data.get('activityRate')
+
+    # print("Airborne Rate:", airborne_rate)
+    # print("Mask Rate:", mask_rate)
+    # print("Activity Rate:", activity_rate)
+
+    return jsonify({'message': 'Simulation started successfully.'}), 200
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -8,13 +8,35 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var airborneRateControl = document.getElementById('airborne-rate');
 var maskRateControl = document.getElementById('mask-rate');
 var activityRateControl = document.getElementById('activity-rate');
+const startBtn = document.getElementById('start-simulation');
 
-function handleControlChange() {
+function startSimulation() {
     var airborneRate = parseFloat(airborneRateControl.value);
     var maskRate = parseFloat(maskRateControl.value);
     var activityRate = parseFloat(activityRateControl.value);
+
+    fetch('/start-simulation', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            airborneRate: airborneRate,
+            maskRate: maskRate,
+            activityRate: activityRate
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Simulation started successfully.');
+            // You can add further actions here if needed
+        } else {
+            console.error('Failed to start simulation.');
+        }
+    })
+    .catch(error => {
+        console.error('Error starting simulation:', error);
+    });
 }
 
-airborneRateControl.addEventListener('input', handleControlChange);
-maskRateControl.addEventListener('input', handleControlChange);
-activityRateControl.addEventListener('input', handleControlChange);
+startBtn.addEventListener('click', startSimulation);
