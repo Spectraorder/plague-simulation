3ecccf129d76b69e3c900b76945ad28d92f0afc8
Spectraorder <2421036333@qq.com>
2024-02-20 11:38:46 -0500

    daily contact input

--- static/js/script.js
+++ static/js/script.js
@@ -4,29 +4,28 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// var heatData = generateRandomHeatData(1000); // Generate 1000 random data points
-
-// // Create heatmap layer
-// var heat = L.heatLayer(heatData, {radius: 25, gradient: {0.2: 'blue', 0.5: 'lime', 0.7: 'yellow', 1: 'red'}}).addTo(map);
-
-// // Function to generate random data points
-// function generateRandomHeatData(count) {
-//     var data = [];
-//     for (var i = 0; i < count; i++) {
-//         data.push([
-//             Math.random() * 180 - 90, // Random latitude between -90 and 90
-//             Math.random() * 360 - 180, // Random longitude between -180 and 180
-//             Math.random() // Random intensity between 0 and 1
-//         ]);
-//     }
-//     return data;
-// }
-
 
 var airborneRateControl = document.getElementById('airborne-rate');
 var maskRateControl = document.getElementById('mask-rate');
 var activityRateControl = document.getElementById('activity-rate');
+
+const airborneValueDisplay = document.getElementById('airborne-value');
+const maskValueDisplay = document.getElementById('mask-value');
+const activityValueDisplay = document.getElementById('activity-value');
+
 const startBtn = document.getElementById('start-simulation');
+
+airborneRateControl.addEventListener('input', function() {
+    airborneValueDisplay.textContent = airborneRateControl.value;
+});
+
+maskRateControl.addEventListener('input', function() {
+    maskValueDisplay.textContent = maskRateControl.value;
+});
+
+activityRateControl.addEventListener('input', function() {
+    activityValueDisplay.textContent = activityRateControl.value;
+});
 
 function startSimulation() {
     var airborneRate = parseFloat(airborneRateControl.value);
@@ -47,7 +46,6 @@ function startSimulation() {
     .then(response => {
         if (response.ok) {
             console.log('Simulation started successfully.');
-            // You can add further actions here if needed
         } else {
             console.error('Failed to start simulation.');
         }
